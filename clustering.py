4cdb18a58737cebdce7af951e8e7031d4344cf7e
andrewy2020 <andrewy2020@gmail.com>
2025-02-17 20:18:09 -0500

    made height segmentation cleaner

--- clustering.py
+++ clustering.py
@@ -1,12 +1,9 @@
-import pandas as pd
 import numpy as np
 from pathlib import Path
-import os
 import scipy.io as sio
 import scipy
 import matplotlib.pyplot as plt
-import torch
-from torch.utils.data import Dataset, DataLoader
+from numpy.polynomial.polynomial import Polynomial
 
 from matplotlib.animation import FuncAnimation
 import cv2 as cv
@@ -24,10 +21,12 @@ from visualization import plot_heatmap_spatial
 # insole_data = sio.loadmat(insole_data_path / "gait_recording_112024_walk4.mat")
 
 insole_data_path = Path("data/080624")
-insole_data = sio.loadmat(insole_data_path / "gait_recording_080624_walk2.mat")
+insole_data = sio.loadmat(insole_data_path / "gait_recording_080624_walk3.mat")
 
 
-def convert_insole_spatial(insoleAll_l, insoleAll_r):
+def convert_insole_spatial(
+    insoleAll_l: np.ndarray, insoleAll_r: np.ndarray
+) -> tuple[np.ndarray, np.ndarray]:
     # from Sam's MATLAB files
     # use FORTRAN row-major reordering
     insole_spatial_l = insoleAll_l.reshape((-1, 64, 16), order="F")
@@ -48,17 +47,103 @@ insole_spatial_l, insole_spatial_r = convert_insole_spatial(
     insole_data["insoleAll_l"], insole_data["insoleAll_r"]
 )
 
+# plot_heatmap_spatial(insole_spatial_r, insole_spatial_l)
 
-plot_heatmap_spatial(insole_spatial_r, insole_spatial_l)
+
+def compute_height_seg_coords(
+    insole_spatial: np.ndarray, validate: bool = True, show_plot: bool = False
+) -> np.ndarray:
+    # mean across frames and columns
+    colwise_mean = insole_spatial.mean(axis=(0, 2))
+
+    # smoothing filter
+    # polyorders 2 and 3 have same effect because they share a center
+    # window_length > 5 causes noticable drift in extrema locations
+    smoothed = scipy.signal.savgol_filter(colwise_mean, window_length=5, polyorder=2)
+
+    # invert to get min instead of max
+    valleys, valley_props = scipy.signal.find_peaks(-smoothed, prominence=0)
+
+    # get the most prominent valleys in any order
+    # invert to get max instead of min
+    NVALLEYS = 2
+    valleys_mask = np.argpartition(-valley_props["prominences"], NVALLEYS)[:NVALLEYS]
+
+    peaks = None
+    peak_props = None
+
+    if validate:
+        NPEAKS = 3
+        peaks, peak_props = scipy.signal.find_peaks(smoothed, prominence=0)
+        peaks_mask = np.argpartition(-peak_props["prominences"], NPEAKS)[:NPEAKS]
+
+        peak_valley_sorted = np.sort(
+            np.hstack((peaks[peaks_mask], valleys[valleys_mask]))
+        )
+
+        # we want the ordering to be peak, valley, peak, valley, peak
+        if not np.all(np.diff(np.isin(peak_valley_sorted, peaks[peaks_mask]))):
+            raise Exception("validation failed")
+
+    if show_plot:
+        plt.plot(colwise_mean, label="colwise mean")
+        plt.plot(smoothed, label="smoothed")
+
+        plt.plot(valleys, smoothed[valleys], "o", label="valleys")
+        plt.vlines(
+            x=valleys,
+            ymin=smoothed[valleys],
+            ymax=valley_props["prominences"] + smoothed[valleys],
+            color="C2",
+        )
+
+        if peaks is not None and peak_props is not None:
+            plt.plot(peaks, smoothed[peaks], "x", label="peaks")
+            plt.vlines(
+                x=peaks,
+                ymin=smoothed[peaks] - peak_props["prominences"],
+                ymax=smoothed[peaks],
+                color="C3",
+            )
+
+        plt.legend()
+        plt.show()
+
+    return valleys[valleys_mask]
+
+
+compute_height_seg_coords(insole_spatial_l, validate=False, show_plot=True)
+
+#############################################
+##### clean up to here
+
 # %%
 
 col_line = insole_spatial_l.mean(axis=(0, 2))
-valleys, _ = scipy.signal.find_peaks(-col_line)
+
+smoothed = scipy.signal.savgol_filter(col_line, 5, 2)
+
+plt.plot(col_line)
+plt.plot(smoothed)
+
+plt.show()
+
+valleys, _ = scipy.signal.find_peaks(
+    -col_line,
+)
+peaks, props = scipy.signal.find_peaks(smoothed, prominence=0)
 valleys = sorted(valleys, key=lambda idx: col_line[idx])[:2]
+
+plt.plot(smoothed)
+for peak in peaks:
+    plt.axvline(peak)
+
+plt.show()
 
 plt.plot(col_line)
 for valley in valleys:
     plt.axvline(valley)
+
 plt.show()
 
 heatmap = insole_spatial_l.mean(axis=0)
